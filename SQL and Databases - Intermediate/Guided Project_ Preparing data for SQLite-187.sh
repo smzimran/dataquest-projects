95ce1c06d7c0e3ad47d13fcda739bd907281cdb6
smzimran <30407086+smzimran@users.noreply.github.com>
2017-12-06 14:09:12 +0500

    Add files via upload

---
+++ SQL and Databases - Intermediate/Guided Project_ Preparing data for SQLite-187.sh
@@ -0,0 +1,33 @@
+## 1. Introduction to the data ##
+
+
+
+
+## 2. Filtering the data ##
+
+
+
+
+## 3. Cleaning up the Won? and Unnamed columns ##
+
+
+
+
+## 4. Cleaning up the Additional Info column ##
+
+
+
+
+## 5. Exporting to SQLite ##
+
+
+
+
+## 6. Verifying in SQL ##
+
+
+
+
+## 7. Next steps ##
+
+
